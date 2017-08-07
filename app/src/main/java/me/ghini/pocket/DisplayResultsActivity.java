b919cf96426021705a049ef2e6e2d05c7b1ead4e
Mario Frasca <mario@anche.no>
2017-08-07 16:03:04 -0500

    somewhat fail safe. will not crash at the first failure.

--- app/src/main/java/me/ghini/pocket/DisplayResultsActivity.java
+++ app/src/main/java/me/ghini/pocket/DisplayResultsActivity.java
@@ -22,7 +22,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.io.File;
@@ -38,24 +37,37 @@ public class DisplayResultsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String plantCode = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        String fullPlantCode = getString(R.string.NOTFOUND) + " " + plantCode;
+        String family = "";
+        String species = "";
+        String acqDate = "";
+        String source = "";
         String filename = new File(getExternalFilesDir(null), "pocket.db").getAbsolutePath();
-        SQLiteDatabase database = openOrCreateDatabase(filename, MODE_PRIVATE, null);
-        Cursor resultSet = database.rawQuery("select s.family, s.genus, s.epithet, a.code, p.code "  +
-                "from species s, accession a, plant p " +
-                "where p.accession_id = a._id and a.species_id = s._id and a.code = '" + plantCode + "'", null);
-        resultSet.moveToFirst();
-        String family = resultSet.getString(0);
-        String species = resultSet.getString(1) + " " + resultSet.getString(2);
-        String fullPlantCode = resultSet.getString(3) + resultSet.getString(4);
-        resultSet.close();
+        try {
+            SQLiteDatabase database = openOrCreateDatabase(filename, MODE_PRIVATE, null);
+            Cursor resultSet = database.rawQuery("select s.family, s.genus, s.epithet, a.code, p.code " +
+                    "from species s, accession a, plant p " +
+                    "where p.accession_id = a._id and a.species_id = s._id and a.code = '" + plantCode + "'", null);
+            resultSet.moveToFirst();
+            family = resultSet.getString(0);
+            species = resultSet.getString(1) + " " + resultSet.getString(2);
+            fullPlantCode = resultSet.getString(3) + resultSet.getString(4);
+            resultSet.close();
+        } catch (Exception e) {
+
+        }
 
         // Capture the layout's TextView and set the string as its text
+        TextView tvAccession = (TextView) findViewById(R.id.tvAccession);
+        tvAccession.setText(fullPlantCode);
         TextView tvFamily = (TextView) findViewById(R.id.tvFamily);
         tvFamily.setText(family);
         TextView tvSpecies = (TextView) findViewById(R.id.tvSpecies);
         tvSpecies.setText(species);
-        TextView tvAccession = (TextView) findViewById(R.id.tvAccession);
-        tvAccession.setText(fullPlantCode);
+        TextView tvAcqDate = (TextView) findViewById(R.id.tvAcqDate);
+        tvAcqDate.setText(acqDate);
+        TextView tvSource = (TextView) findViewById(R.id.tvSource);
+        tvSource.setText(source);
     }
 
 }
