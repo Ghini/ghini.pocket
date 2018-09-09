42937f31640d466bd4b407fe4d68001d9ece5608
mfrasca <mario@anche.no>
2018-09-08 21:20:44 -0500

    tentatively add DesktopClientActivity+layout. #32.

---
+++ app/src/main/java/me/ghini/pocket/DesktopClientActivity.java
@@ -0,0 +1,13 @@
+package me.ghini.pocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DesktopClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desktop_client);
+    }
+}
